a95ab7650dff06be970bf74417da07533546ad56
saelra <rasaelee@gmail.com>
2023-12-01 19:19:20 -0800

    Profile, ProfileManager testing

--- Profile.java
+++ Profile.java
@@ -1,2 +1,62 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Profile {
+    private String name;
+    private String status;
+    private List<Profile> friendsList;
+
+    public Profile(String name) {
+        this.name = name;
+        this.friendsList = new ArrayList<>();
+    }
+
+    public Profile(String name, String status) {
+        this.name = name;
+        this.status = status;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+
+    public List<Profile> getFriendsList() {
+        return friendsList;
+    }
+
+    public void addFriends(Profile friend) {
+        friendsList.add(friend);
+    }
+
+    public void printProfile() {
+        System.out.println("Name: " + name);
+        System.out.println("Status: " + status);
+
+        System.out.println("Friends:");
+
+        if (friendsList.isEmpty()) {
+            System.out.println("No friends yet.");
+        } else {
+            for (Profile friend : friendsList) {
+                System.out.println("Name: " + friend.getName());
+                System.out.println("Status: " + getStatus());
+                System.out.println();
+            }
+        }
+    }
 }
--- ProfileManager.java
+++ ProfileManager.java
@@ -1,2 +1,35 @@
+import ADTPackage.StackInterface;
+import GraphPackage.*;
+
+import java.util.Stack;
+
 public class ProfileManager {
+    private UndirectedGraph<Profile> graph;
+
+    public ProfileManager() {
+        graph = new UndirectedGraph<>();
+    }
+    public void addProfile(Profile profile) {
+        graph.addVertex(profile);
+    }
+    public void connectProfiles(Profile profile1, Profile profile2) {
+        graph.addEdge(profile1, profile2);
+    }
+//    public void displayAllProfiles(Stack<Profile> profiles) {
+//        System.out.println("All Profiles:");
+//        while (!profiles.isEmpty()) {
+//            System.out.println(profiles.pop());
+//        }
+//    }
+    public void displayAllProfiles() {
+        StackInterface<Profile> profiles = graph.getTopologicalOrder();
+        System.out.println("All Profiles:");
+        while (!profiles.isEmpty()) {
+            System.out.println(profiles.pop());
+        }
+    }
+
+        public void displayFriend(Profile current) {
+        graph.getBreadthFirstTraversal(current);
+    }
 }
